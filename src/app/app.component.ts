cd069b9eafe560d8fe8a23833b4bb09a967c4769
Raul Ciucalau <ciucalauraul@gmail.com>
2025-11-22 09:05:37 +0100

    Add HeroSection component and integrate it into the app layout; include Handjet and Poppins font files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/header/header.component";
 import { FooterComponent } from './shared/footer/footer.component';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, HeroSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
