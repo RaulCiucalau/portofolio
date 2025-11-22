8af6093861701f99450fcee453d1b91c360a0bc1
Raul Ciucalau <ciucalauraul@gmail.com>
2025-11-22 18:48:12 +0100

    Add WhyMeSection component and update app layout; enhance HeroSection styles and content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/header/header.component";
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { WhymeSectionComponent } from './whyme-section/whyme-section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, HeroSectionComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, HeroSectionComponent, WhymeSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
