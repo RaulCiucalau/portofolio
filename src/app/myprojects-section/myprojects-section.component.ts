06502ac3f9f0eec11cbcbf565d9a2f1ab27ecc78
Raul Ciucalau <ciucalauraul@gmail.com>
2025-11-23 17:40:25 +0100

    Add MyProjectsSection and MySkillsSection components with respective templates

---
+++ src/app/myprojects-section/myprojects-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-myprojects-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './myprojects-section.component.html',
+  styleUrl: './myprojects-section.component.scss'
+})
+export class MyprojectsSectionComponent {
+
+}
